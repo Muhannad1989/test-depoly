e679cb9d5e2cffaf56cd4f695c55e1308f9fd923
unknown <muhannadjudah@gmail.com>
2019-10-31 23:22:25 +0100

    add files and preparimg for deploy

---
+++ my-app-1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>deploy is succeeded</p>
+    </div>
+  );
+}
+
+export default App;
