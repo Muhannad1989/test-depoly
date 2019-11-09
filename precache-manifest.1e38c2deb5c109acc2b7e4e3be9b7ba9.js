397f53d74d16ec793902e686765cf492de243d5a
unknown <muhannadjudah@gmail.com>
2019-11-09 10:44:29 +0100

    Updates

---
+++ precache-manifest.1e38c2deb5c109acc2b7e4e3be9b7ba9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1f808e0574d4138b27b63d03a88cf3",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "2058cef55d6183cd2a0c",
+    "url": "/test-deploy/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "0d934d13680d334d8ec4",
+    "url": "/test-deploy/static/js/2.bd479808.chunk.js"
+  },
+  {
+    "revision": "2058cef55d6183cd2a0c",
+    "url": "/test-deploy/static/js/main.99be4d8f.chunk.js"
+  },
+  {
+    "revision": "5e748d35f86ad7a336d2",
+    "url": "/test-deploy/static/js/runtime-main.f42e4ac8.js"
+  }
+]);
